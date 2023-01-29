656fa038225e57907c1a1ea6514cb55b4faf6f73
sloth <gkwkgo321@gmail.com>
2023-01-29 12:53:00 +0900

    [eslint] Cannot read config file: C:\vue2023\Vue\project\.eslintrc.json해결할것

---
+++ project/src/views/1_databinding/DataBindingStringView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <!-- data함수 안에서 message키와 userName키를 사용해 데이터 가져오기 -->
+    <h1> {{message}} {{userName}} How are you?</h1>
+    <p></p>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      userName: 'Sloth',
+      message: 'Hi!',
+      arr: [],
+      obj: []
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
